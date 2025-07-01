f8beb634dea98a0c15a93f4b3b8dd95503a0cc82
rishi-2406 <ladrishi24@gmail.com>
2025-07-01 19:03:50 +0530

    Added suggested users excluding to follow list

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js";
+import Notification from "../models/notification.model.js";
 
 export const getUserProfile = async (req, res) => {
   const { username } = req.params;
@@ -24,15 +25,59 @@ export const followUnfollowUser = async (req, res) => {
 
   try {
     if (currentUser.following.includes(id)) {
-      await User.findByIdAndUpdate(id, { $pull: { followers: currentUser._id } });
-      await User.findByIdAndUpdate((currentUser._id), { $pull: { following: id } });
+      await User.findByIdAndUpdate(id, {
+        $pull: { followers: currentUser._id },
+      });
+      await User.findByIdAndUpdate(currentUser._id, {
+        $pull: { following: id },
+      });
       res.status(201).json({ message: "Unfollowed user successfully" });
     } else {
-      await User.findByIdAndUpdate(id, { $push: { followers: currentUser._id } });
-      await User.findByIdAndUpdate((currentUser._id) , { $push: { following: id } });
+      await User.findByIdAndUpdate(id, {
+        $push: { followers: currentUser._id },
+      });
+      await User.findByIdAndUpdate(currentUser._id, {
+        $push: { following: id },
+      });
       res.status(201).json({ message: "Followed user successfully" });
     }
+
+    const newNotification = new Notification({
+      from: req.user._id,
+      to: id,
+      type: "follow",
+    });
+
+    await newNotification.save();
   } catch (e) {
     res.status(500).json({ error: `Cannot follow user : ${e.message}` });
   }
 };
+
+export const getSuggestedUsers = async (req, res) => {
+  try {
+    const currentuserId = req.user._id;
+    const followingUsers = await User.findById(currentuserId).select(
+      "following"
+    );
+    const users = await User.aggregate([
+      {
+        $match: {
+          _id: { $ne: currentuserId },
+        },
+      },
+      { $sample: { size: 10 } },
+    ]);
+
+    console.log(followingUsers);
+    const filterdUserList = users.filter((user) => !followingUsers.following.includes(user._id.toString()))
+    const suggestedUsers = filterdUserList.slice(0,4);
+
+    suggestedUsers.map((eachUser) => eachUser.password = null);
+
+    res.status(200).json(suggestedUsers);
+
+  } catch (e) {
+    res.status(500).json({error : e.message})
+  }
+}; 
