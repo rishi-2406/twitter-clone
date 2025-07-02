633fc0f6d6d7f1fac0f8ae7ae7518bf0a00351d6
rishi-2406 <ladrishi24@gmail.com>
2025-07-02 19:06:38 +0530

    Added like and comment to post

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -31,16 +31,100 @@ export const createPost = async (req, res) => {
 };
 
 export const deletePost = async (req, res) => {
-    try {
-        const deletedPost = await Post.findByIdAndDelete(req.params.id);
-        if(deletedPost) {
-            return res.status(200).json({message : "Post deleted successfully", deletedPost})
-        } else {
-            return res.status(500).json({error : "Post not found"});
-        }
+  try {
+    const postToBeDeleted = await Post.findById(req.params.id);
 
-    } catch (error) {
-        return res.status(500).json({error : error.message, in : "delete post controller"})
+    if (!postToBeDeleted) {
+      return res.status(500).json({ error: "Post doesnt exist" });
     }
-}
 
+    if (postToBeDeleted.user.toString() !== req.user._id.toString()) {
+      return res
+        .status(500)
+        .json({ error: "You are not authoriszed to delete this post" });
+    }
+
+    if (postToBeDeleted.img) {
+      await v2.uploader.destroy(
+        postToBeDeleted.img.split("/").pop().split(".")[0]
+      );
+    }
+
+    const deletedPost = await Post.findByIdAndDelete(req.params.id);
+    if (deletedPost) {
+      return res
+        .status(200)
+        .json({ message: "Post deleted successfully", deletedPost });
+    } else {
+      return res.status(500).json({ error: "Post not found" });
+    }
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ error: error.message, in: "delete post controller" });
+  }
+};
+
+export const commentPost = async (req, res) => {
+  try {
+    const postId = req.params.id;
+    const { text } = req.body;
+
+    const postToUpdate = await Post.findById(postId);
+
+    if (!postToUpdate) {
+      return res.status(500).json({ error: "Post doesnt exist" });
+    }
+
+    const newComment = {
+      text,
+      user: req.user._id,
+    };
+
+    postToUpdate.comments.push(newComment);
+    await postToUpdate.save();
+
+    return res
+      .status(201)
+      .json({ message: "Comment created successfully", newComment });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Error while making comment", e: error.message });
+  }
+};
+
+export const likePost = async (req, res) => {
+  try {
+    const postId = req.params.id;
+
+    const postToUpdate = await Post.findById(postId);
+
+    if (!postToUpdate) {
+      return res.status(500).json({ error: "Post doesnt exist" });
+    }
+
+    if (req.user._id.toString() === postToUpdate.user.toString()) {
+      return res.status(500).json({ error: "Cant like your own post" });
+    }
+
+    let message = "Liked Successfully";
+
+    if (postToUpdate.likes.includes(req.user._id.toString())) {
+      postToUpdate.likes = postToUpdate.likes.filter(
+        (userLiked) => userLiked.toString() !== req.user._id.toString()
+      );
+      message = "Unliked Successfully";
+    } else {
+      postToUpdate.likes.push(req.user._id);
+    }
+
+    await postToUpdate.save();
+
+    return res.status(201).json({ message });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Error while liking", e: error.message });
+  }
+};
--- backend/models/post.model.js
+++ backend/models/post.model.js
@@ -17,7 +17,6 @@ const postSchema = new mongoose.Schema(
       {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User",
-        default : []
       },
     ],
     comments: [
@@ -31,8 +30,7 @@ const postSchema = new mongoose.Schema(
           ref : "User",
           required: true,
         },
-        default : []
-      },
+      },  
     ],
   },
   { timestamps: true }
