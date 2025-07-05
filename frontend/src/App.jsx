7c63c14846e9d41d6de4b7d4bd6c1d7859df9b0a
rishi-2406 <ladrishi24@gmail.com>
2025-07-05 19:01:11 +0530

    Added basic frontend structure without backend logic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,21 @@
+import Login from "./pages/auth/login/Login";
+import SignUp from "./pages/auth/signup/SignUp";
+import HomePage from "./pages/home/HomePage";
 
-export default function App() {
 
-  return (
-    <>
-      
-    </>
-  )
-}
+import { Route, Routes } from "react-router-dom";
+
+function App() {
+	return (
+		<div className='flex max-w-6xl mx-auto'>
+			
+			<Routes>
+				<Route path='/' element={<HomePage />} />
+				<Route path='/signup' element={<SignUp/>} />
+				<Route path='/login' element={<Login />} />
+			</Routes>
+		</div>
+	);
+} 
+
+export default App;
